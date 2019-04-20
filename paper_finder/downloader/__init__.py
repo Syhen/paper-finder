6da6cdeb91abbb9004a1ae1f0cda55f29f899bb5
heyao <heyao@haimaqingfan.com>
2019-04-21 01:54:32 +0800

    add: add downloader quick import

--- paper_finder/downloader/__init__.py
+++ paper_finder/downloader/__init__.py
@@ -4,3 +4,5 @@ create on 2019-04-21 上午1:26
 
 author @heyao
 """
+from paper_finder.downloader.cnki import CnkiPaperDownloader
+from paper_finder.downloader.science_direct import ScienceDirectPaperDownloader
